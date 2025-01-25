6ffbc28acad902c4d21ebd513d00991c84545e83
Matheus Paizante <119441065+MPaizante@users.noreply.github.com>
2025-01-25 12:16:44 -0300

    .

--- JAVAPOO/src/Introducao/Aula01/tiposPrimitivosExercicios.java
+++ JAVAPOO/src/Introducao/Aula01/tiposPrimitivosExercicios.java
@@ -1,4 +1,10 @@
 package Introducao.Aula01;
 
 public class tiposPrimitivosExercicios {
+    public static void main(String [] args){
+     String nome = "Matheus";
+     String endereco = "RJ";
+     float salariof = 2550.55f;
+     System.out.println("Eu " + nome + " no endereço "+ endereco+ " recebi o salario de "+salariof+" reais.");
+    }
 }
