7030eeee0e3f1427c054a8c631ffd8a90c6b5aff
Matheus Paizante <119441065+MPaizante@users.noreply.github.com>
2025-01-26 18:38:48 -0300

    Update EstruturasCondicionais.java

--- JAVAPOO/src/Introducao/Aula01/EstruturasCondicionais.java
+++ JAVAPOO/src/Introducao/Aula01/EstruturasCondicionais.java
@@ -33,8 +33,67 @@ public class EstruturasCondicionais {
         }
         System.out.println(mensagem);
 
+        float salAnual = 45000f;
+        float primeiraFaixa = 9.70f / 100f;
+        float segundaFaixa = 37.35f / 100f;
+        float terceiraFaixa = 49.50f /100f;
+        float valorImposto;
+        if(salAnual <= 34712){
+            valorImposto = salAnual *primeiraFaixa;
 
+        } else if (salAnual >34712 && salAnual <= 68507) {
+            valorImposto = salAnual * segundaFaixa;
+        }else {
+            valorImposto = salAnual * terceiraFaixa;
+        }
+        System.out.println(valorImposto);
 
+        byte dia = 1;
+        switch (dia){
+            case 1:
+                System.out.println("Domingo!");
+                break;
+            case 2:
+                System.out.println("Segunda");
+                break;
+            case 3:
+                System.out.println("Terça");
+                break;
+            case 4:
+                System.out.println("Quarta");
+                break;
+            case 5:
+                System.out.println("Quinta");
+                break;
+            case 6:
+                System.out.println("Sexta");
+                break;
+            case 7:
+                System.out.println("Sábado");
+                break;
 
+            default:
+                System.out.println("Erro!");
+                break;
+
+        }
+        char sexo = 'M';
+        switch (sexo){
+            case 'M':
+                System.out.println("Homem");
+                break;
+            case 'F':
+                System.out.println("Mulher");
+                break;
+            case 'T':
+                System.out.println("Trasngenero");
+                break;
+            case 'N':
+                System.out.println("Prefiro não dizer!");
+                break;
+            default:
+                System.out.println("Ainda não atualizamos, mas iremos inserir mais generos.");
+                break;
+        }
     }
 }
