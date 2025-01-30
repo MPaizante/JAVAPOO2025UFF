dff302c9e9c2f37e6dfc6de9cdf1c9f5dd264ee8
Matheus Paizante <119441065+MPaizante@users.noreply.github.com>
2025-01-30 20:29:10 -0300

    .

---
+++ JAVAPOO/src/Introducao/ModificadorEstatico/dominio/Carro.java
@@ -0,0 +1,50 @@
+package Introducao.ModificadorEstatico.dominio;
+
+public class Carro {
+    private String nome;
+    private double velMax;
+    private static  double velLimite = 250;
+
+    public void imprime(){
+        System.out.println("----------------------------------------------------------------------");
+        System.out.println("O carro "+this.nome + " de velocidade maxima "+this.getVelLimite() + " só pode ir até " + this.velMax+"KM/h.");
+        System.out.println("----------------------------------------------------------------------");
+    }
+    public Carro(String nome, double velMax ){
+        this.nome = nome;
+        this.velMax = velMax;
+
+    }
+    public Carro(){
+
+    }
+
+
+
+
+
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public double getVelLimite() {
+        return velLimite;
+    }
+
+    public double getVelMax() {
+        return velMax;
+    }
+
+    public void setVelMax(double velMax) {
+        this.velMax = velMax;
+    }
+
+
+
+
+}
