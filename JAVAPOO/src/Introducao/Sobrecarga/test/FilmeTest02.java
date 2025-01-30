09e0465f86e538ae4cbdd9628574e8ee40508945
Matheus Paizante <119441065+MPaizante@users.noreply.github.com>
2025-01-29 22:14:46 -0300

    .

---
+++ JAVAPOO/src/Introducao/Sobrecarga/test/FilmeTest02.java
@@ -0,0 +1,13 @@
+package Introducao.Sobrecarga.test;
+
+
+import Introducao.Sobrecarga.dominio.Filme02;
+
+public class FilmeTest02 {
+    public static void main(String[] args) {
+        Filme02 filme = new Filme02("Velozes e Furiosos","Cinema" , 11, "Ação");
+
+        filme.imprime();
+
+    }
+}
