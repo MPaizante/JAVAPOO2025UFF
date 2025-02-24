071c3c86960d6c42bebad5599a78cc15c53466e8
Matheus Paizante <119441065+MPaizante@users.noreply.github.com>
2025-02-24 17:52:17 -0300

    .

--- JAVAPOO/src/Introducao/ModificardorFinal/dominio/Carro.java
+++ JAVAPOO/src/Introducao/ModificardorFinal/dominio/Carro.java
@@ -5,11 +5,17 @@ public class Carro {
     public final Comprpador COMPRADOR = new Comprpador("Matheus");
     public static final double VELOCIDADE_LIMITE = 250;
 
+
     public Carro(String nome){
         this.nome = nome;
     }
     public String getNome() {
         return nome;
+    }
+
+    public final void imprime(){
+        System.out.println(this.nome);
+        System.out.println(this.COMPRADOR.getNome());
     }
 
 }
--- JAVAPOO/src/Introducao/ModificardorFinal/test/TestCarro01.java
+++ JAVAPOO/src/Introducao/ModificardorFinal/test/TestCarro01.java
@@ -2,6 +2,7 @@ package Introducao.ModificardorFinal.test;
 
 import Introducao.ModificardorFinal.dominio.Carro;
 import Introducao.ModificardorFinal.dominio.Comprpador;
+import Introducao.ModificardorFinal.dominio.Ferrari;
 
 public class TestCarro01 {
     public static void main(String[] args) {
@@ -10,5 +11,7 @@ public class TestCarro01 {
         System.out.println(carro.VELOCIDADE_LIMITE);
         System.out.println(carro.getNome());
         System.out.println(carro.COMPRADOR.getNome());
+        Ferrari ferrari = new Ferrari("Abbbbb");
+        ferrari.imprime();
     }
 }
