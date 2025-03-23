1e999b7120da76d88514e23b3927e714970afe7f
Matheus Paizante <119441065+MPaizante@users.noreply.github.com>
2025-03-23 15:06:44 -0300

    Create WapperTest01.java

---
+++ JAVAPOO/src/Introducao/Warpper/WapperTest01.java
@@ -0,0 +1,12 @@
+package Introducao.Warpper;
+
+public class WapperTest01 {
+    public static void main(String[] args) {
+        Byte p1 = 1;
+        Short p2 = 1;
+        Long p3 = 1L;
+        Integer p4 = 1;
+        Double p5 = 1D;
+        Float p6 = 1F;
+    }
+}
