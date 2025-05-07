27eb9199fe1014f0e298ca4a95846f959a95f898
Matheus Paizante <119441065+MPaizante@users.noreply.github.com>
2025-05-06 21:41:00 -0300

    Update myFunction.java

--- JAVAPOO/src/DUKE_UC_SANDIEGO/Solving_Problems_with_Software/myFunction.java
+++ JAVAPOO/src/DUKE_UC_SANDIEGO/Solving_Problems_with_Software/myFunction.java
@@ -17,9 +17,27 @@ public class myFunction {
     public void imprime(){
         System.out.println(funcFinal());
     }
-
+    int h( int x, int y){
+        if(x <y) {
+            System.out.println("x < y");
+            return x*y;
+        }
+        else {
+            System.out.println("x > = y");
+            if(x>8){
+                return y+7;
+            }
+        }
+        return x-2;
+    }
+     int t(){
+        int a = h(3,4);
+        int b = h(a,5);
+        return b;
+    }
     public static void main(String[] args) {
         myFunction valor = new myFunction();
         valor.imprime();
+        valor.t();
     }
 }
