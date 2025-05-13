a60df39b3c64a6c2ba6c5c3204e9d2dd2a4c0f0b
Matheus Paizante <119441065+MPaizante@users.noreply.github.com>
2025-05-13 19:43:56 -0300

    Create FindGeneSimpleAndTest.java

---
+++ JAVAPOO/src/DUKE_UC_SANDIEGO/Solving_Problems_with_Software/FindGeneSimpleAndTest.java
@@ -0,0 +1,40 @@
+package DUKE_UC_SANDIEGO.Solving_Problems_with_Software;
+
+public class FindGeneSimpleAndTest {
+    public String findGeneSimple(String dna){
+        String result = "";
+        int startIndex = dna.indexOf("ATG");
+        int stopIndex = dna.indexOf("TAA", startIndex+3);
+        result = dna.substring(startIndex,stopIndex + 3);
+        return result;
+    }
+    public void testFindGeneSimple(){
+        String dna = "AATGCGTAATATGGT";
+        System.out.println("DNA strand is "+dna);
+        String gene = findGeneSimple(dna);
+        System.out.println("Gene is "+gene);
+        System.out.println("----------------");
+        dna = "AATGCTAGGGTAATATGGT";
+        System.out.println("DNA strand is " + dna);
+        gene = findGeneSimple(dna);
+        System.out.println("Gene is "+gene);
+        System.out.println("----------------");
+        dna = "ATCCTATGCTTCGGCTGCTCTAATATGGT";
+        System.out.println("DNA strand is " + dna);
+        gene = findGeneSimple(dna);
+        System.out.println("Gene is "+gene);
+        System.out.println("----------------");
+        dna = "ATGTAA";
+        System.out.println("DNA strand is " + dna);
+        gene = findGeneSimple(dna);
+        System.out.println("Gene is "+gene);
+        System.out.println("----------------");
+    }
+
+    public static void main(String[] args) {
+        FindGeneSimpleAndTest teste = new FindGeneSimpleAndTest();
+        teste.testFindGeneSimple();
+        
+    }
+
+}
