c7b6d752788f5fc7c6d4366e61c8caa1e9ab8ab9
Matheus Paizante <119441065+MPaizante@users.noreply.github.com>
2025-05-13 17:57:32 -0300

    Create Aula01.java

---
+++ JAVAPOO/src/DUKE_UC_SANDIEGO/Solving_Problems_with_Software/Aula01.java
@@ -0,0 +1,9 @@
+package DUKE_UC_SANDIEGO.Solving_Problems_with_Software;
+
+public class Aula01 {
+    public static void main(String[] args) {
+        String s = "dukeprogramming";
+        String x = s.substring(0,s.length());
+        System.out.println(x + s.indexOf(""));
+    }
+}
