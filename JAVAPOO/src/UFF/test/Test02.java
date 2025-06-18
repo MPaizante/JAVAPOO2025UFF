f49046c9eca4b2c0dea5100b38c0a6845ee815d2
Matheus Paizante <119441065+MPaizante@users.noreply.github.com>
2025-06-18 19:08:18 -0300

    Update Test02.java

--- JAVAPOO/src/UFF/test/Test02.java
+++ JAVAPOO/src/UFF/test/Test02.java
@@ -1,4 +1,16 @@
 public class Test02{
+public static boolean hasLetter(String word, char letter){
+    for(int i = 0; i<word.length();i++){
+        if (word.charAt(i) == letter){
+            return true;
+        }
 
+    }
+    return false;
+}
+public static void main(String[] args) {
+    boolean verdeiro = hasLetter("Matheus", "a");
+
+}
 
 }
