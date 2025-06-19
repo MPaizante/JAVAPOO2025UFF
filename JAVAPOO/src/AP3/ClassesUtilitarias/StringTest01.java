1823256c69e97e08998da963900bc69555658067
Matheus Paizante <119441065+MPaizante@users.noreply.github.com>
2025-06-19 20:26:55 -0300

    .

--- JAVAPOO/src/AP3/ClassesUtilitarias/StringTest01.java
+++ JAVAPOO/src/AP3/ClassesUtilitarias/StringTest01.java
@@ -1,4 +1,19 @@
 package AP3.ClassesUtilitarias;
 
 public class StringTest01 {
+    public static void main(String[] args) {
+        String nome = "Matheus";
+        String nome2 = "Matheus";
+        System.out.println(nome2.equals(nome));
+        System.out.println(nome2 == nome);
+        nome = nome.concat(" Paizante");
+        System.out.println(nome);
+        String nome3 = new String("Matheus");
+        System.out.println(nome2 == nome3);
+        System.out.println(nome2.equals(nome3));
+        System.out.println(nome2 == nome3.intern());
+
+
+
+    }
 }
