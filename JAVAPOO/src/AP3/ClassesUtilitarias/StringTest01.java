3ad0c0568d7194c2a71b1a770240da684480d206
Matheus Paizante <119441065+MPaizante@users.noreply.github.com>
2025-06-19 20:16:49 -0300

    .

---
+++ JAVAPOO/src/AP3/ClassesUtilitarias/StringTest01.java
@@ -0,0 +1,4 @@
+package AP3.ClassesUtilitarias;
+
+public class StringTest01 {
+}
---
+++ JAVAPOO/src/AP3/ClassesUtilitarias/WrapperTest01.java
@@ -0,0 +1,26 @@
+package AP3.ClassesUtilitarias;
+
+public class WrapperTest01 {
+    public static void main(String[] args) {
+        byte byteP = 1;
+        short shortP = 1;
+        int intP = 1;
+        long longP = 10L;
+        float floatP = 10F;
+        double doubleP = 10D;
+        char chatP = 'M';
+        boolean booleanP = true;
+
+        Byte byteW = 127;
+        Short shortW = 1;
+        Integer intW = 1;
+        Long longW = 10L;
+        Float floatW = 10F;
+        Double doubleW = 10D;
+        Character chatW = 'M';
+        Boolean booleanW = true;
+
+
+
+    }
+}
