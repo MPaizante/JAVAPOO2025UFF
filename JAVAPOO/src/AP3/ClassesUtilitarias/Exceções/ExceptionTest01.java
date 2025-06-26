8e64d84ecbecabe97a8335f034bdc3dcbdae1a10
Matheus Paizante <119441065+MPaizante@users.noreply.github.com>
2025-06-25 22:13:48 -0300

    .

--- JAVAPOO/src/AP3/ClassesUtilitarias/Exceções/ExceptionTest01.java
+++ JAVAPOO/src/AP3/ClassesUtilitarias/Exceções/ExceptionTest01.java
@@ -1,4 +1,25 @@
 package AP3.ClassesUtilitarias.Exceções;
 
+import java.io.File;
+
 public class ExceptionTest01 {
+    public static void main(String[] args) {
+        criarNovoArquivo();
+    }
+    private static void criarNovoArquivo(){
+        File file = new File("aquivo.txt");
+        try{
+           boolean isCriado = file.createNewFile();
+            System.out.println(isCriado);
+            int [] nums = {1,2};
+            System.out.println(nums[3]);
+
+        }catch (Exception e){
+            System.out.println("Erro!" + e);
+            e.printStackTrace();
+            System.out.println("Erro!" + e);
+
+        }
+
+    }
 }
