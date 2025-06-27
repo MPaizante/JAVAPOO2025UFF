0651614b0ab2a412643d9493bb8ec3fd112618ef
Matheus Paizante <119441065+MPaizante@users.noreply.github.com>
2025-06-26 22:58:28 -0300

    Update CalculadoraImposto.java

--- JAVAPOO/src/AP3/ClassesUtilitarias/Polimorfismo/CalculadoraImposto.java
+++ JAVAPOO/src/AP3/ClassesUtilitarias/Polimorfismo/CalculadoraImposto.java
@@ -1,16 +1,6 @@
 package AP3.ClassesUtilitarias.Polimorfismo;
 
 public class CalculadoraImposto{
-    public static void calcularImpostoComputador(Computador computador){
-        double imposto = computador.calcularImposto();
-        System.out.println("Relatorio de imposto do Computador: "+imposto);
-    }
-
-    public static void calcularImpostoTomate(Tomate tomate){
-        double imposto = tomate.calcularImposto();
-        System.out.println("Relatorio de imposto do Tomate: "+imposto);
-    }
-
     public static void calcularImposto(Produto produto){
         System.out.println("Relatorio de imposto do produto "+produto.getNome());
         System.out.println("Calculando...");
